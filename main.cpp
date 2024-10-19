b41ff16882001f63587826e9c28da5e936410600
dhykas <mdhika987@gmail.com>
2024-10-19 12:53:18 +0700

    nilai terbesar

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+main()
+{
+    int a, b, c;
+    cout << "masukan nilai ke-1 : ";
+    cin >> a;
+    cout << "masukan nilai ke-2 : ";
+    cin >> b;
+    cout << "masukan nilai ke-3 : ";
+    cin >> c;
+    if (a > b && a > c)
+    {
+        
+        cout << "\nNilai terbesar Adalah : " << a;
+    }
+    else if (b > a && b > c)
+    {
+        cout << "\nNilai terbesar Adalah : " << b;
+    }
+    else
+    {
+        cout << "\nNilai terbesar Adalah : " << c;
+    }
+}
