604321786a4f0e4ed1bba716acc96a3338b30a13
dhykas <mdhika987@gmail.com>
2024-10-26 13:20:43 +0700

    fix forloop

--- ganjilgenap.cpp
+++ ganjilgenap.cpp
@@ -3,14 +3,14 @@ using namespace std;
 
 int main() {
     cout << "Ganjil: ";
-    int i = 1;
-    while (i <= 35) {
+    
+    for (int i = 1; i <= 35; i += 2) { 
         cout << i << " ";
-        i += 2; 
     }
     
     cout << "\nGenap: ";
-    i = 2; 
+
+    int i = 2; 
     while (i <= 35) {
         cout << i << " ";
         i += 2; 
